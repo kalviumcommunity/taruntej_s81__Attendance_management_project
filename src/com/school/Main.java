1725cd9c236fc2e9a8c364beebb8bed5c3318b52
tarun-tej-2007 <taruntej947@gmail.com>
2025-07-15 11:40:28 +0530

    feat: Initial project setup (Part 1)

--- src/com/school/Main.java
+++ src/com/school/Main.java
@@ -3,5 +3,7 @@ package com.school;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the Attendance System!");
+        // Initialize the attendance system
     }
 }
+
